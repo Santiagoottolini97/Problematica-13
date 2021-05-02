c49ed721f9f1c4b8aea8a3d1692e62a681efdcab
santiago <santiago.ottolini97@hotmail.com>
2021-05-02 14:00:00 -0300

    P13: Mock change value

--- operations.mock.js
+++ operations.mock.js
@@ -1,16 +1,14 @@
 import 'jest';
-let isNumber = jest.fn().mockReturnValue(1);
-let isArray = jest.fn().mockReturnValue([]);
-let isString = jest.fn().mockReturnValue('string');
-let validateEmail = jest.fn().mockReturnValue('san@gmail.com');
+let isNumber = jest.fn().mockReturnValue(true);
+let isArray = jest.fn().mockReturnValue(true);
+let isString = jest.fn().mockReturnValue(true);
+let validateEmail = jest.fn().mockReturnValue(true);
 let sum = jest.fn().mockReturnValue(18);
 let multip = jest.fn().mockReturnValue(20);
 let division = jest.fn().mockReturnValue(2);
 let exponent = jest.fn().mockReturnValue(3);
-let isSupportedFigure = jest.fn().mockReturnValue('triangle');
-let arrayElementsAreObjectWithKey = jest
-    .fn()
-    .mockReturnValue([{ zoomGlass: '1,75' }, { zoomGlass: '2,70' }, { zoomGlass: '3' }]);
+let isSupportedFigure = jest.fn().mockReturnValue(true);
+let arrayElementsAreObjectWithKey = jest.fn().mockReturnValue(true);
 let arrayIntersection = jest.fn().mockReturnValue([101]);
 let sortArrayByKey = jest.fn().mockReturnValue([{ name: 'Romina' }, { name: 'Joaquin' }, { name: 'Luciano' }], 'name');
 let getEvenNumbersFromArray = jest.fn().mockReturnValue([12, 24]);
@@ -19,6 +17,7 @@ let getOddNumbersFromArray = jest.fn().mockReturnValue([9, 15]);
 const funmock = jest.mock('./basicOperations', () => {
     return {
         sum,
+        isNumber,
         isNumber,
         isArray,
         isString,
